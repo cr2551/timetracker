ba60bae9b27c865f92d5a85d585476c6f682d93f
cr2551 <92417699+cr2551@users.noreply.github.com>
2023-08-20 22:14:21 -0500

    worked on implementing stopwatch feature

--- timetracker.sh
+++ timetracker.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-#Track the time spent on projects
-# set auto tracking: when a command is execute ex: vim .vimrc start session "vimrc" which will track how much time you spend tweaking your .vimrc
-
 
 PROGDIR=~/.TimeTracker
 # create directory if it does not exist
@@ -44,19 +41,25 @@ close_sess() {
     total_time=$(( $(date +%s) - $(cat "$session") ))
     project=$( ls "$PROGDIR"/ | grep  -o "session-.*" | awk -F - '{print $(NF)}')
     echo $total_time >> "$PROGDIR"/"$project"
-    rm "$PROGDIR"/session-* 
+    rm "$PROGDIR"/session-*
 }
 
 start_sess() {
     date +%s >> "$PROGDIR"/session-"$project"
-}    
+}
 
 
+# function to kill stopwatch timer
+kill_watch() {
+    kill $(ps -e | grep "stopwatch.sh" | awk {'print $1'})
+    #overwrite the file with an empty string
+    echo "" > "$PROGDIR/stopwatch"
+}
 
 
 
 # s option: start session
-# takes the argument project and calls the start_sess func which 
+# takes the argument project and calls the start_sess func which
 # creates a file that starts with the word "session"
 if [ "$1" == "-s" ]; then
     # checks if there is a session already running
@@ -69,19 +72,30 @@ if [ "$1" == "-s" ]; then
             echo "provide a project name"
             exit
         fi
+        echo "hello world"
+        return "hi"
         start_sess && echo "Started session for $project"
+        # redirecting the output to /dev/null is essential, even though the output was already going to the stopwatch file in PROGDIR
+        stopwatch.sh > /dev/null &
         exit
+
     else
         echo "A Session is already running"
         exit
     fi
 
-# c option: close session    
+
+
+# -c option: close session
 # this option does not need an argument passed to it. It will look for the file that has the word session in it.
 elif [[ "$1" == "-c" ]]; then
+
+    kill_watch
+    #kill $(ps -e | grep "stopwatch.sh" | awk {'print $1'})
     session=$(find "$PROGDIR"/ -regex ".*session.*")
 # if there are more than 2 files that match the regex, then the next check will fail
     if [[ -f "$session" ]]; then
+        #kill_stopwatch
         close_sess
         echo "-c $project" >> "$PROGDIR"/.last
         echo "session  $project  closed"
@@ -92,15 +106,15 @@ elif [[ "$1" == "-c" ]]; then
         exit
     fi
 
-#delete time    
+#delete time
 #elif [[ "$1" == "-d" ]]; then
 #    amount="$2"
 #    project="$3"
 #    if [ "$amount" =~ "\d" ] && [ -z "$project" ]; then
-#        show_time 
-#        echo $(( $total - $amount)) >> $project" 
+#        show_time
+#        echo $(( $total - $amount)) >> $project"
 #    fi
-    
+
 
 #remove last session
 elif [[ "$1" == "-r" ]]; then
@@ -117,16 +131,16 @@ elif [[ "$1" == "-r" ]]; then
         exit
     fi
 #delete last saved session needs more work
-elif [[ "$1" == "-d" ]]; then 
+elif [[ "$1" == "-d" ]]; then
     project=$PROGDIR/$2
     head -n-1 $project > $project
 
-# option p:  pause or resume session    
-# does not yet support projects with spaces. 
+# option p:  pause or resume session
+# does not yet support projects with spaces.
 elif [[ "$1" == "-p" ]]; then
 
     session=$(find "$PROGDIR"/ -regex ".*session.*")
-    
+
     if [[ -f $session ]]; then
         close_sess
         echo "session  $project  paused"
